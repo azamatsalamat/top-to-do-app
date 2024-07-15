89fa0c614ffdf26c2410d8624c838b2c81161323
Azamat Salamat <azamat.salamat@samgau.com>
2024-07-15 21:43:38 +0500

    use Prettier

--- src/components/new-task-menu/new-task.js
+++ src/components/new-task-menu/new-task.js
@@ -1,68 +1,70 @@
-import newTaskHtml from './new-task.html';
-import { Priorities } from '../../script/task';
-import { createTask, getProjectByName } from '../../script';
-import { showTaskDetails } from '../view-task/view-task';
-import { showProjectTasks } from '../view-project/view-project';
+import newTaskHtml from "./new-task.html";
+import { Priorities } from "../../script/task";
+import { createTask, getProjectByName } from "../../script";
+import { showTaskDetails } from "../view-task/view-task";
+import { showProjectTasks } from "../view-project/view-project";
 
-export function showNewTaskMenuDOM(){
-    const newTaskMenu = document.getElementById('newTaskMenu');
-    if (newTaskMenu.innerHTML == ''){
-        newTaskMenu.innerHTML = newTaskHtml;
+export function showNewTaskMenuDOM() {
+  const newTaskMenu = document.getElementById("newTaskMenu");
+  if (newTaskMenu.innerHTML == "") {
+    newTaskMenu.innerHTML = newTaskHtml;
 
-        const prioritySelect = document.getElementById('newTaskPriority');
-        Object.entries(Priorities).forEach(([key, value]) => {
-            const option = document.createElement('option');
-            option.value = value;
-            option.textContent = key;
-            prioritySelect.appendChild(option);
-        });
+    const prioritySelect = document.getElementById("newTaskPriority");
+    Object.entries(Priorities).forEach(([key, value]) => {
+      const option = document.createElement("option");
+      option.value = value;
+      option.textContent = key;
+      prioritySelect.appendChild(option);
+    });
 
-        const cancelButton = document.getElementById('newTaskCancelButton');
-        cancelButton.addEventListener('click', hideNewTaskMenuDOM);
+    const cancelButton = document.getElementById("newTaskCancelButton");
+    cancelButton.addEventListener("click", hideNewTaskMenuDOM);
 
-        const form = document.getElementById('newTaskMenuForm');
-        form.addEventListener('submit', createNewTask);
-    }
+    const form = document.getElementById("newTaskMenuForm");
+    form.addEventListener("submit", createNewTask);
+  }
 }
 
-function hideNewTaskMenuDOM(){
-    const newTaskMenu = document.getElementById('newTaskMenu');
-    newTaskMenu.innerHTML == '';
+function hideNewTaskMenuDOM() {
+  const newTaskMenu = document.getElementById("newTaskMenu");
+  newTaskMenu.innerHTML == "";
 }
 
-function createNewTask(event){
-    event.preventDefault();
+function createNewTask(event) {
+  event.preventDefault();
 
-    const projectName = document.getElementById('project-title').textContent;
-    const title = document.getElementById('newTaskText').value.trim();
-    const desc = document.getElementById('newTaskDesc').value.trim();
-    const deadline = document.getElementById('newTaskDeadline').value;
-    const priority = document.getElementById('newTaskPriority').value;
-    
-    const task = createTask(projectName, title, desc, priority, deadline);
+  const projectName = document.getElementById("project-title").textContent;
+  const title = document.getElementById("newTaskText").value.trim();
+  const desc = document.getElementById("newTaskDesc").value.trim();
+  const deadline = document.getElementById("newTaskDeadline").value;
+  const priority = document.getElementById("newTaskPriority").value;
 
-    addNewTaskToDom(task);
+  const task = createTask(projectName, title, desc, priority, deadline);
+
+  addNewTaskToDom(task);
 }
 
-export function addNewTaskToDom(task){
-    const tasksList = document.getElementById('tasks-list');
-    const newTaskElement = document.createElement('li');
+export function addNewTaskToDom(task) {
+  const tasksList = document.getElementById("tasks-list");
+  const newTaskElement = document.createElement("li");
 
-    const newTaskElementContent = document.createElement('span');
-    newTaskElementContent.textContent = task.title;
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.onclick = () => deleteTask(task);
+  const newTaskElementContent = document.createElement("span");
+  newTaskElementContent.textContent = task.title;
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = () => deleteTask(task);
 
-    newTaskElement.appendChild(newTaskElementContent);
-    newTaskElement.appendChild(deleteButton);
+  newTaskElement.appendChild(newTaskElementContent);
+  newTaskElement.appendChild(deleteButton);
 
-    newTaskElementContent.onclick = () => showTaskDetails(task);
-    tasksList.appendChild(newTaskElement);
+  newTaskElementContent.onclick = () => showTaskDetails(task);
+  tasksList.appendChild(newTaskElement);
 }
 
-function deleteTask(task){
-    const project = getProjectByName(document.getElementById('project-title').textContent);
-    project.removeTask(task);
-    showProjectTasks(project);
+function deleteTask(task) {
+  const project = getProjectByName(
+    document.getElementById("project-title").textContent
+  );
+  project.removeTask(task);
+  showProjectTasks(project);
 }
